63f932c1faaf93b27cd5f10bb84c1329592f13c3
dhruvesh <geetakhidake2211@gmail.com>
2022-06-19 17:36:06 +0530

    First Commit

---
+++ String assignment.js
@@ -0,0 +1,16 @@
+var test = "Learning Javascript with AlmaBetter";
+
+console.log(test.length);
+console.log(test.slice(25,35));
+console.log(test.replace("Javascript","Web3.0"));
+console.log(test.toUpperCase())
+console.log(test.concat("Web3.0 is future"));
+
+/*
+Output
+35
+AlmaBetter
+Learning Web3.0 with AlmaBetter
+LEARNING JAVASCRIPT WITH ALMABETTER
+Learning Javascript with AlmaBetterWeb3.0 is future
+*/
