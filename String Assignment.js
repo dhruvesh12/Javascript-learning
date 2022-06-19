2d263204392efc2db3016430e52139a35350cf2c
khidakematrimonial <khidake.matrimonial@gmail.com>
2022-06-19 16:20:56 +0530

    Added With Proper Output

---
+++ String Assignment.js
@@ -0,0 +1,7 @@
+var test = "Learning Javascript with AlmaBetter";
+
+console.log(test.length);
+console.log(test.slice(25,35));
+console.log(test.replace("Javascript","Web3.0"));
+console.log(test.toUpperCase())
+console.log(test.concat("Web3.0 is future"));
