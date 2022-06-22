7c06bdb011e0c6d8d2342d64d963a8c5ed9cd064
dhruvesh <geetakhidake2211@gmail.com>
2022-06-23 00:22:28 +0530

    first commit

---
+++ Array In Javascript/javascript objects and json/Destructuring Syntax.js
@@ -0,0 +1,13 @@
+const userActivity3 = { name: "Rahul",
+    jobTitle: "JS Instructor@almabetter.com",
+    work: "rahul@microsoft.com",
+    region: "Hyderabad" 
+}
+
+const {name,jobTitle,work}=userActivity3
+
+console.log(name,jobTitle,work)
+
+/*
+Rahul JS Instructor@almabetter.com rahul@microsoft.com
+*/
