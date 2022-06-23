942eda6482add5c679a216224135862614e46e00
dhruvesh <geetakhidake2211@gmail.com>
2022-06-24 00:17:27 +0530

    First Commit

---
+++ condition and statement/even_odd_number.js
@@ -0,0 +1,22 @@
+var num3 = 17.5;
+var num4 = 42;
+
+if(num3%2==0){
+    console.log(`${num3} is even`)
+}else{
+
+    console.log(`${num3} is odd`)
+}
+
+if(num4%2!=0){
+    console.log(`${num4} is Odd`)
+}else{
+
+    console.log(`${num4} is Even`)
+}
+
+/*
+17.5 is odd
+42 is Even
+
+*/
