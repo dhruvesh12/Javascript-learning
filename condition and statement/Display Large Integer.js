244b74d2e9aa621604e3e7d149a36bc78dd3da75
dhruvesh <geetakhidake2211@gmail.com>
2022-06-24 00:02:23 +0530

    First Commit

---
+++ condition and statement/Display Large Integer.js
@@ -0,0 +1,14 @@
+var num1 = 5;
+var num2 = 10;
+
+if(num1 > num2){
+
+    console.log(`The larger of ${num1} and ${num2} is ${num1}.`)
+}else{
+    console.log(`The larger of ${num1} and ${num2} is ${num2}.`)
+
+}
+
+/*
+The larger of 5 and 10 is 10.
+*/
