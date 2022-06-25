73ecede5ea979bc24e5f6cf80bcf925486250f70
dhruvesh <geetakhidake2211@gmail.com>
2022-06-26 00:36:32 +0530

    First Commit

---
+++ Loops and iterator/Reverse order of elements.js
@@ -0,0 +1,20 @@
+let arr = [43, "what", 9, true, "cannot", false, "be", 3.5, true];
+
+
+
+for(let i=arr.length-1; i>=0 ; i--){
+
+    console.log(arr[i])
+}
+
+//Output
+
+// true
+// 3.5
+// be
+// false
+// cannot
+// true
+// 9
+// what
+// 43
