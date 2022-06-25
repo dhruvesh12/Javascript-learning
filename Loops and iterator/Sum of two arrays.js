71be03294b754c4c2027e94f7c88465505660784
dhruvesh <geetakhidake2211@gmail.com>
2022-06-26 00:20:46 +0530

    First Commit

---
+++ Loops and iterator/Sum of two arrays.js
@@ -0,0 +1,20 @@
+let arr_1 = [3, 5, 22, 5, 7, 2, 45, 75, 89, 21, 2]; 
+let arr_2 = [9, 2, 42, 55, 71, 22, 4, 5, 90, 25, 26];
+
+let sumOfarr_1= 0
+let sumOfarr_2= 0
+
+for (i of arr_1){
+    sumOfarr_1=sumOfarr_1+i
+}
+
+for (i of arr_2){
+    sumOfarr_2=sumOfarr_2+i
+}
+
+console.log(sumOfarr_1 + sumOfarr_2)
+
+//Output
+// Sum of Two Array is 627
+
+//console.log(sumOfarr_2)
